d44ed42142e2a1e6cbbd61ac92a676f6788cedc5
Hwangtaehun <r283@naver.com>
2024-05-08 21:53:37 +0900

    업로드

--- 7주차/Server/Server/TCPServer_Fixed.cpp
+++ 7주차/Server/Server/TCPServer_Fixed.cpp
@@ -1,13 +1,11 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
-#define _CRT_SECURE_NO_WARNINGS
 #pragma comment(lib, "ws2_32")
 #include <winsock2.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 
 #define SERVERPORT 9000
-#define BUFSIZE    512
+#define BUFSIZE    50
 
 // 소켓 함수 오류 출력 후 종료
 void err_quit(char *msg)
@@ -36,11 +34,29 @@ void err_display(char *msg)
     LocalFree(lpMsgBuf);
 }
 
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET s, char *buf, int len, int flags)
+{
+    int received;
+    char *ptr = buf;
+    int left = len;
+
+    while (left > 0) {
+        received = recv(s, ptr, left, flags);
+        if (received == SOCKET_ERROR)
+            return SOCKET_ERROR;
+        else if (received == 0)
+            break;
+        left -= received;
+        ptr += received;
+    }
+
+    return (len - left);
+}
+
 int main(int argc, char *argv[])
 {
     int retval;
-    char msg[9];
-    bool first = true;
 
     // 윈속 초기화
     WSADATA wsa;
@@ -49,8 +65,7 @@ int main(int argc, char *argv[])
 
     // socket()
     SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-    sprintf_s(msg, "socket");
-    if (listen_sock == INVALID_SOCKET) err_quit(msg);
+    if (listen_sock == INVALID_SOCKET) err_quit("socket()");
 
     // bind()
     SOCKADDR_IN serveraddr;
@@ -59,13 +74,11 @@ int main(int argc, char *argv[])
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons(SERVERPORT);
     retval = bind(listen_sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
-    sprintf_s(msg, "bind");
-    if (retval == SOCKET_ERROR) err_quit(msg);
+    if (retval == SOCKET_ERROR) err_quit("bind()");
 
     // listen()
     retval = listen(listen_sock, SOMAXCONN);
-    sprintf_s(msg, "listen");
-    if (retval == SOCKET_ERROR) err_quit(msg);
+    if (retval == SOCKET_ERROR) err_quit("listen()");
 
     // 데이터 통신에 사용할 변수
     SOCKET client_sock;
@@ -78,8 +91,7 @@ int main(int argc, char *argv[])
         addrlen = sizeof(clientaddr);
         client_sock = accept(listen_sock, (SOCKADDR *)&clientaddr, &addrlen);
         if (client_sock == INVALID_SOCKET) {
-            sprintf_s(msg, "accept");
-            err_display(msg);
+            err_display("accept()");
             break;
         }
 
@@ -87,29 +99,12 @@ int main(int argc, char *argv[])
         printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
             inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 
-        time_t timer;
-        timer = time(NULL);
-        struct tm* t;
-        t = localtime(&timer);
-
-        retval = recv(client_sock, buf, BUFSIZE, 0);
-        buf[retval] = '\0';
-        sprintf(buf, "%d년 %d월 %d일 %d시 %d분 %d초에 접속",
-            t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
-        retval = send(client_sock, buf, strlen(buf) + 1, 0);
-        if (retval == SOCKET_ERROR) {
-            sprintf_s(msg, "accept");
-            err_display(msg);
-            break;
-        }
-
         // 클라이언트와 데이터 통신
         while (1) {
             // 데이터 받기
-            retval = recv(client_sock, buf, BUFSIZE, 0);
+            retval = recvn(client_sock, buf, BUFSIZE, 0);
             if (retval == SOCKET_ERROR) {
-                sprintf_s(msg, "accept");
-                err_display(msg);
+                err_display("recv()");
                 break;
             }
             else if (retval == 0)
@@ -117,16 +112,8 @@ int main(int argc, char *argv[])
 
             // 받은 데이터 출력
             buf[retval] = '\0';
-            printf("[TCP/%s:%d] %s %d\n", inet_ntoa(clientaddr.sin_addr),
-                ntohs(clientaddr.sin_port), buf, strlen(buf));
-
-            // 데이터 보내기
-            retval = send(client_sock, buf, retval, 0);
-            if (retval == SOCKET_ERROR) {
-                sprintf_s(msg, "accept");
-                err_display(msg);
-                break;
-            }
+            printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr),
+                ntohs(clientaddr.sin_port), buf);
         }
 
         // closesocket()
