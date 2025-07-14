d6c5740d2e2b26dd668a290eb5725cb95c81ff94
Hwangtaehun <r283@naver.com>
2025-07-14 19:56:38 +0900

    업로드

--- 서버실습2/3주차/CompletionPortTCPServer/CompletionPortTCPServer.cpp
+++ 서버실습2/3주차/CompletionPortTCPServer/CompletionPortTCPServer.cpp
@@ -1,352 +1,203 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma comment(lib, "ws2_32")
 #include <winsock2.h>
+#include <windows.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <malloc.h>
+#include <vector>
+#include <mutex>
 
 #define SERVERPORT 9000
 #define BUFSIZE    512
 
-bool first;
-int index = 0;
-int *sock_arr;
-char con_msg[36];
-
-// 소켓 정보 저장을 위한 구조체
-struct SOCKETINFO
-{
-	OVERLAPPED overlapped;
-	SOCKET sock;
-	char buf[BUFSIZE + 1];
-	int recvbytes;
-	int sendbytes;
-	WSABUF wsabuf;
+struct SOCKETINFO {
+    OVERLAPPED overlapped;
+    SOCKET sock;
+    char buf[BUFSIZE + 1];
+    int recvbytes;
+    int sendbytes;
+    WSABUF wsabuf;
+    bool sending;
 };
 
-// 작업자 스레드 함수
+std::vector<SOCKETINFO*> clients;
+std::mutex clients_mutex;
+
 DWORD WINAPI WorkerThread(LPVOID arg);
-// 오류 출력 함수
 void err_quit(char* msg);
 void err_display(char* msg);
-bool send(SOCKETINFO* ptr, int retval);
-bool recieve(SOCKETINFO* ptr, int retval);
-void print();
+bool send(SOCKETINFO* ptr);
+bool receive(SOCKETINFO* ptr);
+void broadcast(SOCKETINFO* sender, const char* msg, int len);
+void remove_client(SOCKETINFO* ptr);
 
-int main(int argc, char* argv[])
-{
-	int retval;
+int main(int argc, char* argv[]) {
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
 
-	// 윈속 초기화
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+    HANDLE hcp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+    if (hcp == NULL) return 1;
 
-	// 입출력 완료 포트 생성
-	HANDLE hcp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
-	if (hcp == NULL) return 1;
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    for (int i = 0; i < (int)si.dwNumberOfProcessors * 2; i++) {
+        HANDLE hThread = CreateThread(NULL, 0, WorkerThread, hcp, 0, NULL);
+        CloseHandle(hThread);
+    }
 
-	// CPU 개수 확인
-	SYSTEM_INFO si;
-	GetSystemInfo(&si);
+    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) err_quit("socket()");
 
-	// (CPU 개수 * 2)개의 작업자 스레드 생성
-	HANDLE hThread;
-	for (int i = 0; i < (int)si.dwNumberOfProcessors * 2; i++) {
-		hThread = CreateThread(NULL, 0, WorkerThread, hcp, 0, NULL);
-		if (hThread == NULL) return 1;
-		CloseHandle(hThread);
-	}
+    SOCKADDR_IN serveraddr = { 0 };
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVERPORT);
+    if (bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr)) == SOCKET_ERROR)
+        err_quit("bind()");
 
-	// socket()
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+    if (listen(listen_sock, SOMAXCONN) == SOCKET_ERROR)
+        err_quit("listen()");
 
-	// bind()
-	SOCKADDR_IN serveraddr;
-	ZeroMemory(&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons(SERVERPORT);
-	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
-	if (retval == SOCKET_ERROR) err_quit("bind()");
+    printf("[Chat Server] Listening on port %d...\n", SERVERPORT);
 
-	// listen()
-	retval = listen(listen_sock, SOMAXCONN);
-	if (retval == SOCKET_ERROR) err_quit("listen()");
+    while (1) {
+        SOCKADDR_IN clientaddr;
+        int addrlen = sizeof(clientaddr);
+        SOCKET client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+        if (client_sock == INVALID_SOCKET) {
+            err_display("accept()");
+            continue;
+        }
+        printf("[Client Connected] %s:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 
-	// 데이터 통신에 사용할 변수
-	SOCKET client_sock;
-	SOCKADDR_IN clientaddr;
-	int addrlen;
-	DWORD recvbytes, flags;
+        CreateIoCompletionPort((HANDLE)client_sock, hcp, (ULONG_PTR)client_sock, 0);
 
-	while (1) {
-		// accept()
-		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
-		if (client_sock == INVALID_SOCKET) {
-			err_display("accept()");
-			break;
-		}
-		printf("[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
-			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+        SOCKETINFO* ptr = new SOCKETINFO;
+        ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
+        ptr->sock = client_sock;
+        ptr->recvbytes = ptr->sendbytes = 0;
+        ptr->wsabuf.buf = ptr->buf;
+        ptr->wsabuf.len = BUFSIZE;
+        ptr->sending = false;
 
-		//접속시간 확인
-		time_t timer;
-		timer = time(NULL);
-		struct tm* t;
-		t = localtime(&timer);
-		sprintf(con_msg, "%d년 %d월 %d일 %d시 %d분 %d초에 접속",
-			t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+        {
+            std::lock_guard<std::mutex> lock(clients_mutex);
+            clients.push_back(ptr);
+        }
 
-		// 소켓과 입출력 완료 포트 연결
-		CreateIoCompletionPort((HANDLE)client_sock, hcp, client_sock, 0);
+        receive(ptr);
+    }
 
-		// 소켓 정보 구조체 할당
-		SOCKETINFO* ptr = new SOCKETINFO;
-		if (ptr == NULL) break;
-		ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
-		ptr->sock = client_sock;
-		ptr->recvbytes = ptr->sendbytes = 0;
-		ptr->wsabuf.buf = ptr->buf;
-		ptr->wsabuf.len = BUFSIZE;
-
-		//클라이언트 소켓 정리
-		if (index == 0) {
-			sock_arr = new int[index + 1];
-			sock_arr[index] = ptr->sock;
-		}
-		else {
-			int* temp = new int[index];
-			for (int i = 0; i < index; i++) {
-				temp[i] = sock_arr[i];
-			}
-			delete[] sock_arr;
-
-			sock_arr = new int[index + 1];
-			for (int i = 0; i < index; i++) {
-				sock_arr[i] = temp[i];
-			}
-			sock_arr[index] = ptr->sock;
-			delete[] temp;
-		}
-		index++;
-		//print();
-		//끝
-
-		first = true;
-
-		// 비동기 입출력 시작
-		flags = 0;
-		retval = WSARecv(client_sock, &ptr->wsabuf, 1, &recvbytes,
-			&flags, &ptr->overlapped, NULL);
-		if (retval == SOCKET_ERROR) {
-			if (WSAGetLastError() != ERROR_IO_PENDING) {
-				err_display("WSARecv()");
-			}
-			continue;
-		}
-	}
-
-	// 윈속 종료
-	WSACleanup();
-	delete[] sock_arr;
-	return 0;
+    WSACleanup();
+    return 0;
 }
 
-// 작업자 스레드 함수
-DWORD WINAPI WorkerThread(LPVOID arg)
-{
-	int retval;
-	HANDLE hcp = (HANDLE)arg;
+DWORD WINAPI WorkerThread(LPVOID arg) {
+    HANDLE hcp = (HANDLE)arg;
+    DWORD cbTransferred;
+    SOCKET client_sock;
+    SOCKETINFO* ptr;
+    int retval;
 
-	while (1) {
-		// 비동기 입출력 완료 기다리기
-		DWORD cbTransferred;
-		SOCKET client_sock;
-		SOCKETINFO* ptr;
-		retval = GetQueuedCompletionStatus(hcp, &cbTransferred,
-			(LPDWORD)&client_sock, (LPOVERLAPPED*)&ptr, INFINITE);
+    while (1) {
+        retval = GetQueuedCompletionStatus(hcp, &cbTransferred, (LPDWORD)&client_sock, (LPOVERLAPPED*)&ptr, INFINITE);
+        if (retval == 0 || cbTransferred == 0) {
+            err_display("Client Disconnected");
+            closesocket(ptr->sock);
+            remove_client(ptr);
+            delete ptr;
+            continue;
+        }
 
-		// 클라이언트 정보 얻기
-		SOCKADDR_IN clientaddr;
-		int addrlen = sizeof(clientaddr);
-		getpeername(ptr->sock, (SOCKADDR*)&clientaddr, &addrlen);
+        if (ptr->sending) {
+            ptr->sendbytes += cbTransferred;
+            if (ptr->sendbytes < ptr->recvbytes) {
+                send(ptr);
+            }
+            else {
+                ptr->recvbytes = ptr->sendbytes = 0;
+                ptr->sending = false;
+                receive(ptr);
+            }
+        }
+        else {
+            ptr->recvbytes = cbTransferred;
+            ptr->buf[cbTransferred] = '\0';
 
-		// 비동기 입출력 결과 확인
-		if (retval == 0 || cbTransferred == 0) {
-			if (retval == 0) {
-				DWORD temp1, temp2;
-				WSAGetOverlappedResult(ptr->sock, &ptr->overlapped,
-					&temp1, FALSE, &temp2);
-				err_display("WSAGetOverlappedResult()");
-			}
-			closesocket(ptr->sock);
-			printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
-				inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
-			
-			//추가
-			int* temp = new int[index - 1];
-			int t_idx = 0;
+            SOCKADDR_IN clientaddr;
+            int addrlen = sizeof(clientaddr);
+            getpeername(ptr->sock, (SOCKADDR*)&clientaddr, &addrlen);
 
-			for (int i = 0; i < index; i++) {
-				if (sock_arr[i] != ptr->sock) {
-					temp[t_idx] = sock_arr[i];
-					t_idx++;
-				}
-			}
-			delete[] sock_arr;
-			index--;
+            printf("[Message from %s:%d] %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), ptr->buf);
 
-			sock_arr = new int[index];
-			for (int i = 0; i < index; i++) {
-				sock_arr[i] = temp[i];
-			}
-			delete[] temp;
-
-			//print();
-			//끝
-
-			delete ptr;
-			continue;
-		}
-
-		// 데이터 전송량 갱신
-		if (ptr->recvbytes == 0) {
-			ptr->recvbytes = cbTransferred;
-			ptr->sendbytes = 0;
-
-			ptr->buf[ptr->recvbytes] = '\0';
-			if (!first) {
-				printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr),
-					ntohs(clientaddr.sin_port), ptr->buf);
-			}
-		}
-		else {
-			ptr->sendbytes += cbTransferred;
-		}
-
-		if (ptr->recvbytes > ptr->sendbytes) {
-			//시간입출력확인
-			if (first) {
-				sprintf(ptr->buf, con_msg);
-				ptr->recvbytes = strlen(con_msg);
-				first = false;
-			}
-
-			// 데이터 보내기
-			ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
-			ptr->wsabuf.buf = ptr->buf + ptr->sendbytes;
-			ptr->wsabuf.len = ptr->recvbytes - ptr->sendbytes;
-
-			DWORD sendbytes;
-			retval = WSASend(ptr->sock, &ptr->wsabuf, 1,
-				&sendbytes, 0, &ptr->overlapped, NULL);
-			if (retval == SOCKET_ERROR) {
-				if (WSAGetLastError() != WSA_IO_PENDING) {
-					err_display("WSASend()");
-				}
-				continue;
-			}
-		}
-		else {
-			ptr->recvbytes = 0;
-
-			// 데이터 받기
-			ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
-			ptr->wsabuf.buf = ptr->buf;
-			ptr->wsabuf.len = BUFSIZE;
-
-			DWORD recvbytes;
-			DWORD flags = 0;
-			retval = WSARecv(ptr->sock, &ptr->wsabuf, 1,
-				&recvbytes, &flags, &ptr->overlapped, NULL);
-			if (retval == SOCKET_ERROR) {
-				if (WSAGetLastError() != WSA_IO_PENDING) {
-					err_display("WSARecv()");
-				}
-				continue;
-			}
-		}
-	}
-
-	return 0;
+            broadcast(ptr, ptr->buf, cbTransferred);
+            receive(ptr);
+        }
+    }
+    return 0;
 }
 
-//전송
-bool send(SOCKETINFO* ptr, int retval) {
-	ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
-	ptr->wsabuf.buf = ptr->buf + ptr->sendbytes;
-	ptr->wsabuf.len = ptr->recvbytes - ptr->sendbytes;
+void broadcast(SOCKETINFO* sender, const char* msg, int len) {
+    std::lock_guard<std::mutex> lock(clients_mutex);
+    for (auto client : clients) {
+        if (client->sock == sender->sock) continue;
+        if (client->sending) continue;
+        ZeroMemory(&client->overlapped, sizeof(client->overlapped));
+        memcpy(client->buf, msg, len);
+        client->recvbytes = len;
+        client->sendbytes = 0;
+        client->wsabuf.buf = client->buf;
+        client->wsabuf.len = len;
+        client->sending = true;
 
-	/*printf("send: %s %d\n", ptr->wsabuf.buf, ptr->wsabuf.len);
-	printf("rbyte: %d, sbyte: %d\n", ptr->recvbytes, ptr->sendbytes);*/
-
-	DWORD sendbytes;
-	retval = WSASend(ptr->sock, &ptr->wsabuf, 1,
-		&sendbytes, 0, &ptr->overlapped, NULL);
-	if (retval == SOCKET_ERROR) {
-		if (WSAGetLastError() != WSA_IO_PENDING) {
-			err_display("WSASend()");
-		}
-		return true;
-	}
-	return false;
+        DWORD sendbytes;
+        int retval = WSASend(client->sock, &client->wsabuf, 1, &sendbytes, 0, &client->overlapped, NULL);
+        if (retval == SOCKET_ERROR && WSAGetLastError() != WSA_IO_PENDING)
+            err_display("broadcast WSASend()");
+    }
 }
 
-//받기
-bool recieve(SOCKETINFO* ptr, int retval) {
-	ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
-	ptr->wsabuf.buf = ptr->buf;
-	ptr->wsabuf.len = BUFSIZE;
-
-	/*printf("recieve: %s %d\n", ptr->wsabuf.buf, ptr->wsabuf.len);
-	printf("rbyte: %d, sbyte: %d\n", ptr->recvbytes, ptr->sendbytes);*/
-
-	DWORD recvbytes;
-	DWORD flags = 0;
-	retval = WSARecv(ptr->sock, &ptr->wsabuf, 1,
-		&recvbytes, &flags, &ptr->overlapped, NULL);
-	if (retval == SOCKET_ERROR) {
-		if (WSAGetLastError() != WSA_IO_PENDING) {
-			err_display("WSARecv()");
-		}
-		return true;
-	}
-	return false;
+void remove_client(SOCKETINFO* ptr) {
+    std::lock_guard<std::mutex> lock(clients_mutex);
+    clients.erase(std::remove(clients.begin(), clients.end(), ptr), clients.end());
 }
 
-//출력
-void print() {
-	for (int i = 0; i < index; i++) {
-		printf("sock_arr[%d] = %d\t", i, sock_arr[i]);
-	}
-	printf("\n");
+bool send(SOCKETINFO* ptr) {
+    ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
+    ptr->wsabuf.buf = ptr->buf + ptr->sendbytes;
+    ptr->wsabuf.len = ptr->recvbytes - ptr->sendbytes;
+    DWORD sendbytes;
+    int retval = WSASend(ptr->sock, &ptr->wsabuf, 1, &sendbytes, 0, &ptr->overlapped, NULL);
+    if (retval == SOCKET_ERROR && WSAGetLastError() != WSA_IO_PENDING)
+        err_display("WSASend()");
+    return retval == SOCKET_ERROR;
 }
 
-// 소켓 함수 오류 출력 후 종료
-void err_quit(char* msg)
-{
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, WSAGetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
-	LocalFree(lpMsgBuf);
-	exit(1);
+bool receive(SOCKETINFO* ptr) {
+    ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
+    ptr->wsabuf.buf = ptr->buf;
+    ptr->wsabuf.len = BUFSIZE;
+    DWORD recvbytes, flags = 0;
+    int retval = WSARecv(ptr->sock, &ptr->wsabuf, 1, &recvbytes, &flags, &ptr->overlapped, NULL);
+    if (retval == SOCKET_ERROR && WSAGetLastError() != WSA_IO_PENDING)
+        err_display("WSARecv()");
+    return retval == SOCKET_ERROR;
 }
 
-// 소켓 함수 오류 출력
-void err_display(char* msg)
-{
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, WSAGetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-	printf("[%s] %s", msg, (char*)lpMsgBuf);
-	LocalFree(lpMsgBuf);
+void err_quit(char* msg) {
+    LPVOID lpMsgBuf;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+    MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+    LocalFree(lpMsgBuf);
+    exit(1);
+}
+
+void err_display(char* msg) {
+    LPVOID lpMsgBuf;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+    printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+    LocalFree(lpMsgBuf);
 }
